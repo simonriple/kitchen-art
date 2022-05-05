e044abe1795e67bbe1e9eb8e1b3fb333a9a44274
Simon Riple <simon.riple@knowit.no>
2022-05-05 15:04:34 +0200

    text

--- src/pages/vote.tsx
+++ src/pages/vote.tsx
@@ -65,7 +65,7 @@ const Vote: NextPage = () => {
           <Text textAlign='center'>
             riple.art generates a piece of art every day from user-submitted
             text using VQGAN+CLIP algorithm. Add your suggestion or vote for the
-            suggestion you want the AI to generate.
+            suggestion you want the AI to generate. You have one vote every day
           </Text>
 
           <Heading fontSize='md'>Add a suggestion for tommorrows art</Heading>
