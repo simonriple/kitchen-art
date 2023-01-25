217cd1993156505df36860197c092e17dcfd931d
Simon Riple <simon.riple@knowit.no>
2023-01-25 08:30:28 +0100

    test aws upload

--- src/services/fileStorageProviders/aws.ts
+++ src/services/fileStorageProviders/aws.ts
@@ -1,16 +1,23 @@
 import AWS from 'aws-sdk'
 
-AWS.config.getCredentials(function (err) {
-  if (err) console.log(err.stack)
-  // credentials not loaded
-  else {
-    console.log('Connected to AWS s3 bucket')
-  }
-})
-
-const s3 = new AWS.S3()
+// AWS.config.getCredentials(function (err) {
+//   if (err) console.log(err.stack)
+//   // credentials not loaded
+//   else {
+//     console.log('Connected to AWS s3 bucket')
+//   }
+// })
 
 export const uploadToAWS = async (externalArtId: string, image: Buffer) => {
+  const s3 = new AWS.S3({
+    credentials: {
+      accessKeyId: `${process.env.aws_access_key_id}`,
+      secretAccessKey: `${process.env.aws_secret_access_key}`,
+    },
+  })
+
+  s3.config.getCredentials((err, credentials) => console.log(err, credentials))
+
   return await s3
     .upload({
       Bucket: `${process.env.AWS_S3_BUCKET_NAME}`,
